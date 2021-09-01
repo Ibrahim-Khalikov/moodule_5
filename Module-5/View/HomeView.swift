805a297df75c3ac83643df8caf45ac55b34d1ec5
Ibrahim Khalikov <starlink050@gmail.com>
2021-09-01 07:51:06 +0500

    few

--- Module-5/View/HomeView.swift
+++ Module-5/View/HomeView.swift
@@ -12,7 +12,7 @@ struct HomeView: View {
  @EnvironmentObject var model: ContentModel
     
     var body: some View {
-        Text("Welcome to my new Module 5 Projects...")
+        Text("Wwefvas...")
             .padding()
     }
 }
