3cb7bdab5e8cd621f0053bb994e9d902c55ec3e0
Ibrahim Khalikov <starlink050@gmail.com>
2021-09-15 07:23:43 +0500

    ujuj

--- Module-5/View/LessonDetailView.swift
+++ Module-5/View/LessonDetailView.swift
@@ -26,7 +26,7 @@ struct LessonDetailView: View {
             
 //            if model.hasNextLesson() {
 //
-            Text("Hello worldttiojnntvyvntvvtv t oit regregregthe y yty i take butwd3dw imonkey jetty on eealking true ")
+            Text("Hello worldiukh7g8jttiojnntvyvntvvtv t oit regregregthe y yty i take butwd3dw imonkey jetty on eealking true ")
             
 //                Button(action: {}, label: {
 //
